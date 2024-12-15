a879c5420c86e29eafc2e317ec8136b54e37bc04
max meadowcroft <mmeadowcroft@cc.nmt.edu>
2024-12-15 16:01:49 -0700

    added viewing functionality

--- todo.py
+++ todo.py
@@ -5,7 +5,6 @@ This program is the main program for the code of this ToDo app.
 In it the program will loop, asking you to either select a task to modify, or add a new task.
 
 Still need to implement
-- Adding tasks functionality
 - Modifying tasks functionality
 """
 
@@ -14,17 +13,28 @@ def main():
     todoItems = {}  # Empty dictionary to store items
 
     menu = """
-    ToDo List. Please enter an option:
-    1. View tasks
-    2. Exit
-    """
+ToDo List. Please enter an option:    
+1. View tasks    
+2. Add tasks
+3. Exit
+"""
 
-    while True:  # infinite loop
+    while True:  # main loop
         print(menu)
-        user_input = input(">")
+        user_input = input("> ")
+        # User chooses to view tasks
         if user_input == '1':
-            pass
+            count = 0
+            keys = list(todoItems.keys())
+            values = list(todoItems.values())
+            for i in range(len(keys)):
+                print(f"{i+1}. {keys[i]}\t{"(DONE)" if values[i] else ""}")
+        # User chooses to add tasks
         elif user_input == '2':
+            task_name = input("Please enter a task name\n> ")
+            todoItems[task_name] = False
+        # User chooses to exit
+        elif user_input == '3':
             break
         else:
             print("Invalid input")
